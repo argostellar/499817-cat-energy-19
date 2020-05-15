9e2a62b1eac1ffee20bc56f0ebef5afaea2babca
argostellar <arskud@gmail.com>
2020-05-15 21:40:20 +0700

    Ускоряет вёрстку, завершает оптимизацию

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -100,6 +103,20 @@ gulp.task("html", function() {
     .pipe(gulp.dest("build"));
 });
 
+gulp.task('minify', function () {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task('compress', function () {
+  return pipeline(
+        gulp.src('source/js/*.js'),
+        uglify(),
+        gulp.dest('build/js')
+  );
+});
+
 gulp.task("build", gulp.series(
   "clean",
   "copy",
@@ -107,7 +124,9 @@ gulp.task("build", gulp.series(
   "webp",
   "css",
   "sprite",
-  "html"
+  "html",
+  "minify",
+  "compress"
 ));
 
 gulp.task("start", gulp.series("build", "server"));
